0abb8a57acf04e16b7b0cc11b5bea034c2334005
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-09-28 19:08:21 +0000

    Initial Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
 
+  # get("/", { :controller => "misc", :action => "index" })
+
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+  post("/insert_user_record", { :controller => "users", :action => "create" })
+  post("/update_user_record/:path_id", { :controller => "users", :action => "update" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  post("/insert_photo_record", { :controller => "photos", :action => "create" })
+  post("/update_photo_record/:path_id", { :controller => "photos", :action => "update" })
+  post("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+  post("/comment_photo/:path_id", { :controller => "photos", :action => "comment" })
+
 end
